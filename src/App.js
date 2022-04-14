856113b86b2f032eb2cf3283502961bb0e8b8a55
abc <abc@example.com>
2022-04-14 13:53:43 +0200

    link route

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetup from "./pages/NewMeetup";
-
+import MainNavigation from "./components/layouts/MainNavigation";
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact={true}>
           <AllMeetups />
